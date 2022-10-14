c7db5876e0f8b1e2fa560a684361f98d672a24e6
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-10-14 16:58:09 +0300

    feat: reset and return buttons features

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-export default function Home () {
-  return (
+const Home = () =>
     <div className="flex items-center p-10 mt-32 justify-center">
       <h1 className='text-4xl'>Start searching!</h1>
     </div>
-  )
-};
+
+export default Home
