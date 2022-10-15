02ef28b8b9c0154d9e6b3ae97cb20e28653ed71a
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-10-15 13:33:33 +0300

    feat: random word search feature

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReturnButton from "../../components/ReturnButton/ReturnButton";
 
 const NotFound = () => <div className="flex items-center flex-col p-10 mt-32 justify-center">
-  <h1 className='text-4xl pb-5'>Oops! Page not found</h1>
+  <h1 className='text-4xl pb-3'>Oops! Page not found</h1>
   <ReturnButton/>
 </div>;
 
